82610ac71161b49cfab0e1b4fbe6e0e66ae6ae5b
Xavier Perez <perezxavier918@gmail.com>
2021-10-12 06:09:08 -0600

    Agregar archivos de proyecto.

---
+++ VentasExpress/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentasExpress
+{
+    class Usuario
+    {
+        protected string usuario1;
+
+        public string Usuario1
+        {
+            get { return usuario1; }
+            set { usuario1 = value; }
+        }
+
+        protected string contraseña1;
+
+        public string Contraseña1
+        {
+            get { return contraseña1; }
+            set { contraseña1 = value; }
+        }
+    }
+}
