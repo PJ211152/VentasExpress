00951a2cc4cebe7640d7645f93e6276e92f41bf4
Xavier Perez <perezxavier918@gmail.com>
2021-10-12 06:14:16 -0600

    PrimerDiseño

--- VentasExpress/Form1.Designer.cs
+++ VentasExpress/Form1.Designer.cs
@@ -30,50 +30,62 @@
         {
             this.txb_Usuario = new System.Windows.Forms.TextBox();
             this.txb_Contraseña = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_Usuario = new System.Windows.Forms.Label();
+            this.lbl_Contraseña = new System.Windows.Forms.Label();
+            this.btn_Login = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txb_Usuario
             // 
-            this.txb_Usuario.Location = new System.Drawing.Point(140, 95);
+            this.txb_Usuario.Location = new System.Drawing.Point(77, 75);
             this.txb_Usuario.Name = "txb_Usuario";
             this.txb_Usuario.Size = new System.Drawing.Size(177, 20);
             this.txb_Usuario.TabIndex = 0;
             // 
             // txb_Contraseña
             // 
-            this.txb_Contraseña.Location = new System.Drawing.Point(140, 185);
+            this.txb_Contraseña.Location = new System.Drawing.Point(77, 132);
             this.txb_Contraseña.Name = "txb_Contraseña";
             this.txb_Contraseña.Size = new System.Drawing.Size(177, 20);
             this.txb_Contraseña.TabIndex = 1;
             // 
-            // label1
+            // lbl_Usuario
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(193, 147);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.lbl_Usuario.AutoSize = true;
+            this.lbl_Usuario.Location = new System.Drawing.Point(140, 59);
+            this.lbl_Usuario.Name = "lbl_Usuario";
+            this.lbl_Usuario.Size = new System.Drawing.Size(43, 13);
+            this.lbl_Usuario.TabIndex = 2;
+            this.lbl_Usuario.Text = "Usuario";
             // 
-            // label2
+            // lbl_Contraseña
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(196, 62);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.lbl_Contraseña.AutoSize = true;
+            this.lbl_Contraseña.Location = new System.Drawing.Point(124, 116);
+            this.lbl_Contraseña.Name = "lbl_Contraseña";
+            this.lbl_Contraseña.Size = new System.Drawing.Size(61, 13);
+            this.lbl_Contraseña.TabIndex = 3;
+            this.lbl_Contraseña.Text = "Contraseña";
+            this.lbl_Contraseña.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(127, 181);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(75, 23);
+            this.btn_Login.TabIndex = 4;
+            this.btn_Login.Text = "Ingresar";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 320);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(336, 235);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.lbl_Contraseña);
+            this.Controls.Add(this.lbl_Usuario);
             this.Controls.Add(this.txb_Contraseña);
             this.Controls.Add(this.txb_Usuario);
             this.Name = "Form1";
@@ -87,8 +99,9 @@
 
         private System.Windows.Forms.TextBox txb_Usuario;
         private System.Windows.Forms.TextBox txb_Contraseña;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_Usuario;
+        private System.Windows.Forms.Label lbl_Contraseña;
+        private System.Windows.Forms.Button btn_Login;
     }
 }
 
--- VentasExpress/Form1.cs
+++ VentasExpress/Form1.cs
@@ -21,5 +21,10 @@ namespace VentasExpress
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
